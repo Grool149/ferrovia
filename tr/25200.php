b878f592f088d0573a51bb1b3fcd48e26e8c137e
Grool <no@mail.com>
2026-05-08 18:23:39 +0200

    Branche Free, suite (25100-40100)
    25100, 25150, 25200, 25500, 26000, 27000, 27300, 27500, 37000, 40100

--- tr/25200.php
+++ tr/25200.php
@@ -1,4 +1,4 @@
 <?php
-  header('Location: http://ferrovia.free.fr/tr/25200.html');
+  header('Location: http://ferrovia.free.fr/tr/25200.html', true, 301);
   exit();
 ?>
--- tr/37000.php
+++ tr/37000.php
@@ -1,4 +1,4 @@
 <?php
-  header('Location: http://ferrovia.free.fr/tr/37000.html');
+  header('Location: http://ferrovia.free.fr/tr/37000.html', true, 301);
   exit();
 ?>
